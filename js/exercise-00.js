9032bc3b523ba087b03d227489331b88858abad6
CCP <Cristian.cruz@mataro.epiaedu.cat>
2024-11-13 18:47:49 +0100

    Finish exercise

---
+++ js/exercise-00.js
@@ -0,0 +1,25 @@
+/* 
+   Create a constant variable of each of the following primitive types:
+   - String
+   - Number
+   - Boolean
+   - Null
+   - Undefined
+
+   Log out each variable to the terminal at the end.
+*/
+// In js/exercise-00.js
+
+// Declare constants of each primitive type
+const myString = "Hello, world!";
+const myNumber = 42;
+const myBoolean = true;
+const myNull = null;
+const myUndefined = undefined;
+
+// Log each variable to the terminal
+console.log("String:", myString);
+console.log("Number:", myNumber);
+console.log("Boolean:", myBoolean);
+console.log("Null:", myNull);
+console.log("Undefined:", myUndefined);
---
+++ js/exercise-04.js
@@ -0,0 +1,19 @@
+/*
+    What would each of these lines of code do:
+
+    1. true && console.log("moo");
+    2. false && console.log("moo moo?")
+    3. true || console.log("hello friend");
+    4. false || console.log("bye friend");
+
+    Once you see the results, write in a comment why it works that way
+*/
+
+true && console.log("moo");
+
+false && console.log("moo moo?");
+
+true || console.log("hello friend");
+
+false || console.log("bye friend");
+
